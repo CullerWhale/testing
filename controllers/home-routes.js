29c960bb3bf9a74472afa7b3beda1e5e7f4b9d46
CullerWhale <jcullenharris@gmail.com>
2022-05-22 15:07:24 -0500

    found all sleeps associated with a tag

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,9 +48,46 @@ router.get('/signup', (req, res) => {
 router.get('/create-sleep', withAuth, (req,res) => {
     Tag.findAll({})
     .then(tagData =>{
-        const tag = tagData.map(post => post.dataValues);
+        const tag = tagData.map(post => post.dataValues); //use post.get({plan:true})) it gets rid of other stuff
         res.render('createSleep', { tag, loggedIn: req.session.loggedIn });
     })
 });
 
+// router.get('/sleep/:tag_name', withAuth, (req,res) => {
+
+router.get('/sleep/:tag_name', (req,res) => {
+    Tag.findOne({
+        where: {
+            tag_name: req.params.tag_name
+        }, 
+        include: [
+            {
+                model: Sleep,
+                attributes: [
+                    'title',
+                    'sleep_description',
+                    'hours_slept',
+                    'dream_sw',
+                    'dream_description',
+                    // user_id: req.session.user_id wrong because user who is logged in not who posted
+                ],
+                include: [
+                    {
+                        model: User, 
+                        attributes: [
+                            'username'
+                        ]
+                    }
+                ]
+            }
+        ]
+
+    })
+    .then(tagSleepsData =>  {
+        const tagSleeps = tagSleepsData.get({plain: true});
+        //send data back
+        res.json(tagSleeps);
+    })
+})
+//connect to front end now. 
 module.exports = router;
